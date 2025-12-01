11a2b5acec7c4a1fc14c609e5ab6b2ed683e330b
PawanPawar11 <pawanpawar4369@gmail.com>
2025-12-01 23:54:16 +0530

    feat(auth): add User model with hashed password support feat(auth): implement signup route with validation feat(auth): implement login route and JWT token generation

---
+++ server/src/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// hash password before saving
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// compare passwords
+userSchema.methods.matchPassword = (enteredPassword) => {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
