bb0f98db7618c912c6123a7816d5a504ca632d7f
PawanPawar11 <pawanpawar4369@gmail.com>
2025-12-02 13:19:23 +0530

    chore: add route inside index.js file

--- server/src/index.js
+++ server/src/index.js
@@ -2,18 +2,21 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-connectDB();
-
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
+
+// auth routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
