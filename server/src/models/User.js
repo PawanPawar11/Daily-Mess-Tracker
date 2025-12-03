39d6a4988826da3f4e424ad8932b6e21afd07c57
PawanPawar11 <pawanpawar4369@gmail.com>
2025-12-03 13:16:46 +0530

    fix(auth): correct bcrypt encryption of password

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -20,14 +20,14 @@ const userSchema = new mongoose.Schema({
 });
 
 // hash password before saving
-userSchema.pre("save", function () {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 // compare passwords
-userSchema.methods.matchPassword = function (enteredPassword) {
-  return bcrypt.compare(enteredPassword, this.password);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
